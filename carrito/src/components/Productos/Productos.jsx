dc7e999c7d9c624e1ab0f49155cb02bd8807b856
Martin Alejandro Luboszyc <103965308+AleLuboszyc@users.noreply.github.com>
2023-07-03 16:09:06 -0300

    css cards

--- carrito/src/components/Productos/Productos.jsx
+++ carrito/src/components/Productos/Productos.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import IMG from "../../images/img01.jpg";
 
 const Productos = () => {
   return (
-    <div>
-      <h1>Productos</h1>
-    </div>
+    <>
+      <h1 className='title'>PRODUCTOS</h1>
+      <div className='productos'>
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt='' />
+            </div>
+          </a>
+          <div className="producto__footer">
+            <h1> Title </h1>
+            <p> Categoria </p>
+            <p className='price'>$320</p>
+          </div>
+          <div className="boton">
+            <button className='btn'>
+              Añadir al carrito
+            </button>
+          </div>
+          <div>
+            <a href="#" className='btn'>
+              Vista
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
