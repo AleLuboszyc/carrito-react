8e6e9c2a915c96f600312b0a09d4a415d11f216f
Martin Alejandro Luboszyc <103965308+AleLuboszyc@users.noreply.github.com>
2023-07-03 15:11:41 -0300

    navbar
    creacion de la navbar y desarrollo del carrito

--- carrito/src/App.js
+++ carrito/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './components/Header/Header.jsx';
 import Productos from "./components/Productos/Productos.jsx"
+import './index.css';
+import 'boxicons';
 
 function App() {
   return (
--- carrito/src/components/Header/Header.jsx
+++ carrito/src/components/Header/Header.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import Nike from "../../images/Nike.jpg";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-    </div>
+    <header>
+      <div className="menu">
+        <box-icon name="menu"></box-icon>
+      </div>
+      <a href='#'>
+        <div className="logo">
+          <img src={Nike} alt='logo' width="150"/>
+        </div>
+      </a>
+      <ul>
+        <li>
+          <a href="#">INICIO</a>
+        </li>
+        <li>
+          <a href="#">PRODUCTOS</a>
+        </li>
+      </ul>
+      <div className='cart'>
+        <box-icon name="cart"></box-icon>
+        <span className='item__total'>0</span>
+      </div>
+    </header>
   )
 }
 
